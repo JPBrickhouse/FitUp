6330623360363a1bb1ba8bb4a7f092929a792105
JBrickhouse <jbrickhouse777@gmail.com>
2020-10-05 16:58:01 -0500

    Testing databases with JawsDB and sequelize

--- routes/handlebars_routes.js
+++ routes/handlebars_routes.js
@@ -1,4 +1,3 @@
-const { Router } = require("express");
 // *********************************************************************************
 // handelbars_routes.js - this file offers a set of routes
 // *********************************************************************************
@@ -8,10 +7,23 @@ const { Router } = require("express");
 var express = require("express")
 var router = express.Router();
 
+// Requiring the models
+var db = require("../models");
+
 // Routes
 // =============================================================
-router.get("/", function(req, res) {
-  res.render("index");
+router.get("/", async function (req, res) {
+  var gettingBurgers = await db.sequelize.query("SELECT * FROM burgers");
+  // console.log(gettingBurgers);
+
+  finalArray = []
+  for (var i = 0; i < gettingBurgers.length; i++) {
+    finalArray.push(gettingBurgers[0][i])
+  }
+
+  console.log(finalArray)
+
+  res.render("index",{burgers:finalArray});
 });
 
 module.exports = router;
